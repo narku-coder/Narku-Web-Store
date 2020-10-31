297419ad02c4eba65c3eb08c0b480bf00dcdd484
bcm100 <bcm100@txstate.edu>
2020-10-30 20:49:36 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,144 +1,168 @@
+const mongoose = require('mongoose')
+let url = 'mongodb+srv://dbAdminUser:owner127@cluster1.yf6y8.mongodb.net/storeDatabase?retryWrites=true&w=majority'
+const User = require('./user');
+const Cart = require('./cart');
+const CartItem = require('./cartItem');
+const StoreItem = require('./storeItem');
 const express = require('express');
-const app = express();
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
-let fs = require("fs");
-let text = fs.readFileSync("./randomItems.txt").toString('utf-8');
-let items = text.split("\n");
+let database;
+let app = express();
+app.use(express.json());
+const router = express.Router();
 
-let carts = [];
-let users = [];
-let store = [];
-
-let i = 0;
-while (i < 50)
-{
-    let storeItem = {
-        ID: (i + 1),
-        name: items[i]
-    }
-
-    store.push(storeItem);
-    i++;
+const initDataBase = async () => {
+	database = await mongoose.connect(url);
+	if(database) {
+		app.use(session({
+			secret: 'ASecretThatICantTell',
+			store: new MongoStore({mongooseConnection: mongoose.connection})
+		}));
+		app.use(router);
+		console.log("Successfully connected to database");
+	}
+	else{
+		console.log("Error connecting to my DB");
+	}
 }
-let cartID = 0;
-let userID = 0;
-let cartItemID = 0;
 
-let user1 = {
-    ID: userID++,
-    firstName:"SampleUser",
-    lastName: "random",
-    emailAddress: "sample@email.com"
-};
-
-let cart1 = {
-    ID: cartID++,
-    user: user1,
-    items: []
-};
-
-let cartItem1 =
-{
-    ID: cartItemID++,
-    name: "SampleCartItem",
-    quantity: 5
-};
-
-carts.push(cart1);
-cart1.items.push(cartItem1);
-
-let num = 0;
-
-app.get('/user', (req, res) => {
-    res.send(users);
-});
-
-app.get('/user/:userId', (req, res) => {
-    let user1;
-    user1 = users.find((user) => {
-        return user.ID == req.params.userId;
-    })
-
-    res.send(user1 ? user1 : 404);  
-});
-
-app.post('/user', (req, res) => {
-    let user = req.body;
-    user.ID = num;
-    num++;
-
-    users.push(user);
-    res.send(user);
-});
-
-app.delete('/user/:userId', (req, res) => {
-    let index = users.indexOf(req.params.userId);
-    let user1 = users.splice(index,1);
-    
-    res.send(user1 ? user1 : 404);  
-});
-
-app.get('/user/:userId/cart', (req, res) => {
-    const carts = carts.filter((cart) => {
-        return carts.user.userID === req.params.userId
-    })
-    res.send(carts ? carts : 404)
-});
-
-app.delete('/user/:userId/cart', (req, res) => {
-    const carts1 = carts.filter((cart) => {
-        return cart.user.userID === req.params.userId
-    })
-    
-    let cartLen = carts.length;
-    let i;
-    for (i = 0; i < cartLen; i++)
-    {
-        carts[i].items.length = 0;
-    }
-    res.send(carts ? carts : 404)
-});
-
-app.post('/cart/:cartId/cartItem', (req, res) => {
-    let item;
-    item = store.find(storeItem => {
-        return storeItem.name == req.params.cartItem;
-    });
-
-    if(item != undefined)
-    {
-        let cart;
-        cart = carts.find((cart) => {
-            return cart.cartID === req.params.cartId;
-        })
-
-        let item = req.body;
-        item.ID = cartItemID++;
-
-        cart.items.push(item);
-        res.send(item); 
-    }
-});
-
-app.delete('/cart/:cartId/cartItem/:cartItemId', (req, res) => {
-    let cart1;
-    cart1 = carts.find((cart) => {
-        return cart.ID == req.params.cartId;
-    })
-
-    let index = cart1.items.indexOf(req.params.cartItemId);
-    let item1 = cart1.items.splice(index,1);
-    
-    res.send(item1 ? item1 : 404);
-});
-
-app.get('/StoreItem/:storeItemId', (req, res) => {
-    let item1;
-    item1 = store.find((storeItem) => {
-        return storeItem.ID == req.params.storeItemId;
-    })
-
-    res.send(item1 ? item1 : 404);
-});
+const init = async () => {
+	await initDataBase();
+}
 
 app.listen(8080);
+
+init();
+
+router.get('/user', async (req, res) => {
+	const users = await User.find({});
+	res.send(users);
+});
+
+router.get('/user/:userId', async (req, res) => {
+	const user = await User.findById(req.params.userId);
+    res.send(user ? user : 404);  
+});
+
+router.post('/user', async (req, res) => {
+	let newUser = req.body;
+
+    const newUser2 = {
+		firstName: newUser.firstName,
+		lastName: newUser.lastName,
+		emailAddress: newUser.emailAddress
+	}
+
+	const user = await User.create(newUser2);
+	const newCart = {
+		items: []
+	}
+	const cart = await Cart.create(newCart);
+
+	user.cart = cart;
+	await user.save()
+    res.send(newUser);
+
+});
+
+router.delete('/user/:userId', async (req, res) => {
+	const user = await User.findById(req.params.userId);
+
+	if (user) {
+		await user.remove();
+	  }
+    
+    res.send(user ? user : 404);  
+});
+
+router.get('/user/:userId/cart', async (req, res) => {
+	const user = await User.findById(req.params.userId);
+	const cart1 = user.cart;
+    res.send(cart1 ? cart1 : 404);
+});
+
+
+router.delete('/user/:userId/cart', async (req, res) => {
+    const user = await User.findById(req.params.userId);
+	const cart1 = user.cart;
+
+	if (cart1) {
+		await cart1.remove();
+	  }
+    res.send(cart1 ? cart1 : 404)
+});
+
+router.post('/cart/:cartId/cartItem', async (req, res) => {
+    let item = await StoreItem.find({name: req.body.name});
+
+    if(item)
+    {
+		let currCart = await Cart.findById(req.params.cartId);
+	    let newItem = await CartItem.create(req.body);
+		currCart.items.push(newItem);
+		await currCart.save();
+		await newItem.save();
+
+		res.send(newItem);
+    }
+});
+
+router.delete('/cart/:cartId/cartItem/:cartItemId', async (req, res) => {
+	let currCart = await Cart.findById(req.params.cartId);
+	let currItem;
+	if (currCart) {
+		currItem = await CartItem.findById(req.params.cartItemId)
+		if(currItem)
+		{
+			await currItem.remove()
+		}
+	  }
+	await currCart.save();
+	await currItem.save();
+
+	res.send(currItem);
+});
+
+let numItems;
+let viewItems = [];
+router.get('/StoreItem/:storeItemId', async (req, res) => {
+    let currItem = await StoreItem.findById(req.params.storeItemId);
+	req.session.numCalls++;
+	req.session.items = [];
+	viewItems.push(currItem);
+	req.session.items = viewItems;
+    res.send(currItem ? currItem : 404);
+});
+
+
+router.get('/StoreItem', async (req, res) => {
+	const items = await StoreItem.find({});
+	
+    res.send(items ? items : 404);
+});
+
+router.get('/StoreItem/Recent', async (req, res) => {
+	let numOfCalls = req.session.numCalls;
+	//let num = req.query.num;
+	console.log("num - " + num /*+ " and numOfCalls - " + numOfCalls*/);
+	res.send("num - " + num /*+ " and numOfCalls - " + numOfCalls*/);
+	/*
+	if (num > numOfCalls)
+	{
+		res.send(404 + "Too many items");
+	}
+	else
+	{
+		let i = 0;
+		items = [];
+		while(i < num)
+		{
+			console.log("req.session.items[i] - " + req.session.items[i]);
+			//items.push(req.session.items[i]);
+			i++;
+		}
+		res.send(items);
+	}*/
+});
