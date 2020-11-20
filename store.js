670d693d93edb51c67858fb1a2f6e8b970d16d7f
bcm100 <bcm100@txstate.edu>
2020-11-20 10:52:14 -0600

    Add files via upload

--- store.js
+++ store.js
@@ -1,38 +1,70 @@
-import React from 'react'
-import axios from 'axios'
-const ScrollArea = require('react-scrollbar');
+import React from 'react';
+import axios from 'axios';
+import ScrollArea from 'react-scrollbar';
+import Cart from './cart';
+import './store.css'
 
 class Store extends React.Component {
+
 	constructor(props){
 		super(props);
 		this.state = {
-			items:''
-		}
-		this.getItems = this.getItems.bind(this);
+			items: [],
+			items2: [],
+			product:"",
+			user:"",
+			token: ""
+		};
+		this.addItemtoCart = this.addItemtoCart.bind(this);
 	}
 
-	async getItems() {
-		const products = await axios.get('/StoreItem');
-		this.setState({items: products});
+	async componentDidMount() {
+		await this.getItems();
+		this.setState({user: this.props.person, token: this.props.token});
 	}
+	async getItems() {
+		const products = (await axios.get('http://localhost:8080/StoreItem')).data;
+		const productsList = products.map((product, index) => <li key={index}>{product.name}<button
+		type="button"
+		onClick={this.addItemtoCart(index)}>Add</button></li>);
+		this.setState({items: (<ol>{productsList}</ol>), items2:{products}});
+	}
+
+	addItemtoCart = i => {
+		this.setState({product:this.state.items2[i], clicked:true});
+	  };
 
 	render() {
-		this.getItems();
-		return (
+		console.log("user - " + JSON.stringify(this.state.user) + " token - " + JSON.stringify(this.state.token)+ "In store render");
+		if(this.state.user)
+		{
+			return(
 			<ScrollArea
             speed={0.8}
             className="storeArea"
             contentClassName="storeItems"
             horizontal={false}
             >
-            	<div className="AuthorsList">                
-					<ul>                    
-					<li>{this.state.items}</li>                                   
-					</ul>            
+            	<div class="itemsList">                
+					<ol type="1">{this.state.items}</ol>            
 				</div>
-          </ScrollArea>
-		);
+				<Cart item={this.state.product} person={this.state.user} token={this.state.token}/>
+		  </ScrollArea>);
+		}
+		else{
+			return(
+				<ScrollArea
+				speed={0.8}
+				className="storeArea"
+				contentClassName="storeItems"
+				horizontal={false}
+				>
+					<div class="itemsList">                
+						<ol type="1">{this.state.items}</ol>            
+					</div>
+			  </ScrollArea>);
+		}
 	}
 }
 
-export default Store
+export default Store;
